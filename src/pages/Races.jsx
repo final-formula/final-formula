9e86a7143b97322799dc91961296d40246a0d74e
Bojan75 <milovicbojan@gmail.com>
2026-05-18 14:04:22 +0200

    races try 3

--- src/pages/Races.jsx
+++ src/pages/Races.jsx
@@ -3,32 +3,33 @@ import axios from "axios";
 import Loader from "./components/Loader";
 
 export default function Races() {
-const [races, setRaces] = useState([]);
-const [loading, setLoading] = useState(true);
+    const [races, setRaces] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-useEffect(() => {
-   getRaces();
-}, []);
- 
+    useEffect(() => {
+        getRaces();
+    }, []);
 
 
-const getRaces = async () => {
-        const url ="https://api.jolpi.ca/ergast/f1/2013/results/1.json";
+
+    const getRaces = async () => {
+        const url = "https://api.jolpi.ca/ergast/f1/2013/results/1.json";
         console.log(url)
         const response = await axios.get(url);
         console.log(response.data.MRData)
         setRaces(response.data.MRData);
         setLoading(false);
-        
-       
+
+
 
     };
 
-}
-if(loading){
-    return <Loader/>
+
+if (loading) {
+    return <Loader />
 }
 
-return(
-<h1>RACES</h1>
+return (
+    <h1>RACES</h1>
 );
+}
