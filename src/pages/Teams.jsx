239669a26ae3b16835bf9bbbb9f43b91d6c61ed6
aleksa94jok <aleksa.jokanovic94@outlook.com>
2026-05-18 15:14:17 +0200

    Teams component

---
+++ src/pages/Teams.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react"
+import axios from "axios";
+import Loader from "../components/Loader";
+
+export default function Teams() {
+
+    const [teams, setTeams] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        getTeams();
+    }, []);
+
+    const getTeams = async () => {
+        const url = "https://api.jolpi.ca/ergast/f1/2013/constructorStandings.json";
+        const response = await axios.get(url);
+        console.log(response.data.MRData);
+        setTeams(response.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings);
+        setLoading(false);
+    }
+console.log(response);
+    if(loading) {
+        return <Loader />
+    }
+
+    return (
+        <div>
+            <h2>Constructors Championship</h2>
+            <table>
+                <tr>
+                    <th>Constructors Championship Standings - 2013</th>
+                </tr>
+                <tbody>
+                {teams.map((team, i) => {
+                    return (
+                        <tr key={i}>
+                            <td>{team.position}</td>
+                            <td>{team.Constructor.name}</td>
+                            <td>detail</td>
+                            <td>{team.points}</td>
+                        </tr>
+                    );
+                })}
+                </tbody>
+
+            </table>
+        </div>
+    )
+}
+
+
+
+
+// 'https://api.jolpi.ca/ergast/f1/' + year + '/constructorStandings.json'
