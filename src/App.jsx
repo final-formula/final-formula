a17367aba3fe2ea0133f6e70543a64ac0d64d36e
Lesavac <aslostspirit@gmail.com>
2026-05-21 13:38:57 +0200

    adovan search

--- src/App.jsx
+++ src/App.jsx
@@ -12,30 +12,10 @@ import { useState, useEffect } from 'react'
 import Loader from "./components/Loader"
 
 function App() {
-
   const [flags, setFlags] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [filters, setFilters] = useState([]);
   const [search, setSearch] = useState("");
-  const [users, setUsers] = useState([]);
-
-  // useEffect(() => {
-  //   getUsers();
-  // }, []);
-
-  // useEffect(() => {
-  //   // const matchUsers = users.filter((user) => user.${}.toLowerCase().includes(search.toLocaleLowerCase()) || user.${}.toLowerCase().includes(search.toLocaleLowerCase()));
-  //   console.log(matchUsers);
-  //   setFilters(matchUsers)
-
-  // }, [search, users]);
-
-  // const getUsers = async () => {
-  //   const url = `https://api.jolpi.ca/ergast/f1/2013/constructors/${params.constructorId}/constructorStandings.json`;
-  //   const response = await axios.get(url);
-  //   setUsers(response.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings[0]);
-  //   setLoading(false);
-  // };
 
   useEffect(() => {
     getFlags();
@@ -43,7 +23,6 @@ function App() {
 
   const getFlags = async () => {
     const url = "https://raw.githubusercontent.com/Imagin-io/country-nationality-list/refs/heads/master/countries.json"
-
     const response = await fetch(url);
     const data = await response.json()
     setFlags(data);
@@ -54,11 +33,7 @@ function App() {
     return <Loader />
   }
 
-  console.log(flags);
-
-
   return (
-
     <BrowserRouter>
       <div className="wrapper">
         <nav>
@@ -71,17 +46,13 @@ function App() {
               <Link to="/teams"><img src="../../public/Teams.png" className="nav-img" /> <span>Teams</span></Link>
               <Link to="/races" className="link-races"><img src="../../public/Races1.png" className="nav-img races" /> <span>Races</span></Link>
               <input type="text" value={search} placeholder="Search F1 ..." onChange={(e) => setSearch(e.target.value)} />
-
             </div>
-
-
           </div>
-
 
         </nav>
         <Routes>
           <Route path="/" element={<Home flags={flags} />} />
-          <Route path="/drivers" element={<Drivers flags={flags} />} />
+          <Route path="/drivers" element={<Drivers flags={flags} search={search} />} />
           <Route path="/drivers/details/:driverId" element={<DriverDetails flags={flags} />} />
           <Route path="/races" element={<Races flags={flags} />} />
           <Route path="/races/details/:raceName" element={<RacesDetails flags={flags} />} />
