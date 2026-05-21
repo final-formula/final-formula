b88b8a1ef330fd2cf481218e317269a5cb83bb83
Lesavac <aslostspirit@gmail.com>
2026-05-21 09:33:36 +0200

    Comit home izmena

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,8 @@ export default function Home() {
     return (
         <div className="home">
             <h1>Home</h1>
-            <p> dobrodosli na formulu jedam</p>
+            <p> dobrodosli na formulu jedan</p>
+            <p>Ovde mozete vide sve informacije o Formli 1</p>
 
         </div>
     )
