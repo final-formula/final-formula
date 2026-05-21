819e4c80f6c8304077812cdf4a9ef476e48930ff
Lesavac <aslostspirit@gmail.com>
2026-05-21 09:34:52 +0200

    com

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ export default function Home() {
         <div className="home">
             <h1>Home</h1>
             <p> dobrodosli na formulu jedan</p>
-            <p>Ovde mozete vide sve informacije o Formli 1</p>
+            <p>Ovde mozete videti sve informacije vezane za  Formlu 1</p>
 
         </div>
     )
