39d3125abee9b822062731c823418d216c39297e
Lesavac <aslostspirit@gmail.com>
2026-05-22 13:32:58 +0200

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ export default function Home() {
             <h1>Home</h1>
             <p> dobrodosli na formulu jedan</p>
             <p>Ovde mozete videti sve informacije vezane za  Formlu 1</p>
+            <p>DULE ZA PREDSEDNIKA</p>
 
         </div>
     )
