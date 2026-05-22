eb618aee5bd80a424b2678112a747047ff58453f
final-formula <duskokovac546@gmail.com>
2026-05-22 13:48:15 +0200

    malo pauze

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,16 @@ export default function Home() {
             <h1>Home</h1>
             <p> dobrodosli na formulu jedan</p>
             <p>Ovde mozete videti sve informacije vezane za  Formlu 1</p>
-            <p>DULE ZA PREDSEDNIKA</p>
+
+
+            <div className="veliki">
+                <span><pre>S           E                  P           DJ</pre></span>
+                <br />
+                <span><pre>   T     D     N     I            O     E      U     U</pre></span>
+                <br />
+                <span><pre>      U           T                  B            J</pre></span>
+
+            </div>
 
         </div>
     )
