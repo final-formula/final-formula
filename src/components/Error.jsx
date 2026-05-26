9f55823aa6175020bcfc01c993f73711e260f145
final-formula <duskokovac546@gmail.com>
2026-05-26 15:39:46 +0200

    utorak kraj

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,7 @@
 export default function Error() {
     return (
         <div className="error-container">
-            <h1>NEEEEEEEEEEEMAAAAAAAAAAAAAAAA</h1>
+            <h1>NEEEEEEEEEEEMAAAAAAAAAAAAAAAAaaa</h1>
         </div>
     )
 }
